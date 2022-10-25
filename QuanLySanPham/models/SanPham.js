f845c0238af8c31a930dde84e8e5e9c35ee8d12c
ductri <31211024561@student.isb.edu.vn>
2022-10-25 12:32:31 +0700

    trang detail + form dang ky

---
+++ QuanLySanPham/models/SanPham.js
@@ -0,0 +1,8 @@
+
+ function SanPham(tenSP,gia,hinhAnh,moTa){
+    //Thuộc tính
+    this.tenSP = tenSP;
+    this.gia = gia;
+    this.hinhAnh = hinhAnh;
+    this.moTa = moTa;
+}
--- js/main.js
+++ js/main.js
@@ -88,3 +88,4 @@ $('.blog-carousel').owlCarousel({
         }
     }
 })
+
